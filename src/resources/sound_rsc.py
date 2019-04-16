7ceab9540df0a125e47f855600204cb3f8b4f5b9
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2019-04-16 22:01:09 +0200

    added sounds for galaxian

--- src/resources/sound_rsc.py
+++ src/resources/sound_rsc.py
@@ -8,3 +8,6 @@ class SoundRsc:
     sounds_path = '../resources/sounds/'
     button_click = pygame.mixer.Sound(sounds_path + 'menu/button_click.wav')
     button_switch = pygame.mixer.Sound(sounds_path + 'menu/button_switch.wav')
+
+    enemy_death = pygame.mixer.Sound(sounds_path + 'galaxian/enemy_death.wav')
+    projectile_launch = pygame.mixer.Sound(sounds_path + 'galaxian/projectile_launch.wav')
